6916b62f7d426590553f02a9210e3b772c5b8bd9
Firmo <firmo.campos@unesp.br>
2026-03-27 12:08:33 -0300

    Correção nas rotas de eventos

--- siteapp/urls.py
+++ siteapp/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("sobre/", views.sobre, name="sobre"),
     path("events/", views.events, name="events"),
-    path('view/<int:event.id>', views.view_event, name='view_event'),
+    path('view/<int:event_id>', views.view_event, name='view_event'),
     path("servicos/", views.servicos, name="servicos"),
     path("contato/", views.contato, name="contato"),
 ]
