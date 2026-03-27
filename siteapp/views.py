b853c0db2be0cc85fcda0005dd6ed5b51e85c81b
Firmo <firmo.campos@unesp.br>
2026-03-27 11:58:55 -0300

    Criação das abas de eventos

--- siteapp/views.py
+++ siteapp/views.py
@@ -10,6 +10,22 @@ def sobre(request):
 def servicos(request):
     return render(request, "siteapp/servicos.html")
 
+EVENTS = []
+
+def events(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        data = request.POST.get('data')
+        evento = {'id': len(EVENTS) +1, 'nome': nome, 'data': data}
+        EVENTS.append(evento)
+
+        return redirect('events')
+    return render(request, 'events.html',{'events': EVENTS})
+
+def view_event(request, event_id):
+    evento = next((e for e in EVENTS if e['id'] == event_id), None)
+
+    return render(request, 'view.html', {'evento': evento})
 
 def contato(request):
     return render(request, "siteapp/contato.html")
